c5518827dab1956970fb3920580a35dc479bee6f
matias1379 <52383409+matias1379@users.noreply.github.com>
2020-05-29 12:03:12 -0300

    se creo el metodo que transforma de rgb a hsv

--- la_caja_impresionista/detector_colores.py
+++ la_caja_impresionista/detector_colores.py
@@ -19,6 +19,41 @@ class detector_colores:
         self.g = g
         self.b = b
 
+    def rgb_to_hsv(self):
+        r = r/255.0
+        g = g/255.0
+        b = b/255.0
+        mx = max(r, g, b)
+        mn = min(r, g, b)
+        df = mx-mn
+        s = (df/mx)*100
+        v = mx*100
+        hue = 0
+
+        if ((mx == r) and (s >= 50) and (v >= 30) and (v <= 60)): #es el valor de marron
+            return 370
+        
+        if ((v > 25) and (v <= 60) and (s <= 10)):  #es el valor de gris
+            return 380
+        
+        if (mx==mn) and (v>15) and (v<60):
+            return 380
+        
+        if ((mx >= (226/255)) and (mn >= (226/255))): #es el valor de blanco
+            return 390
+        
+        if mx <= (25/255):  #es el valor de negro (NO DEBE SER TAN NEGRO!)
+            return 400
+    
+        elif mx == r:
+            hue = (60 * ((g-b)/df) + 360) % 360
+        elif mx == g:
+            hue = (60 * ((b-r)/df) + 120) % 360
+        elif mx == b:
+            hue = (60 * ((r-g)/df) + 240) % 360
+        
+        return hue   
+
     
 
 
