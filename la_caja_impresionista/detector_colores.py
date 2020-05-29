7b37fb2bb6a044a79968aa850ec0cc4e1fdd86b5
matias1379 <52383409+matias1379@users.noreply.github.com>
2020-05-29 12:02:41 -0300

    se creo el metodo constructor de detector_colores

---
+++ la_caja_impresionista/detector_colores.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+class detector_colores:
+    
+    r = int
+    g = int
+    b = int
+    mx = int
+    mn = int
+    df = int
+    v = int
+    hue = int
+    hsv = str
+
+
+    def __init__ (self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
+    
+
+
+        
+
+    
