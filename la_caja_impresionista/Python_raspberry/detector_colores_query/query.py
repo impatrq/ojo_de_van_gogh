2ed25414798c72baee3a4216c17281fb85c5fff7
matias1379 <52383409+matias1379@users.noreply.github.com>
2020-06-24 19:04:55 -0300

    Bases de datos con query que pregunta color

--- la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
+++ la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
@@ -1,4 +1,26 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
+import array
 
-dataframe = pd.read_excel('Tabla_colores.xlsx')
+dataframe = pd.read_csv('Tabla_colores.csv')
+
+color_prueba = [230, 210, 40]
+
+if color_prueba[0] == 0:
+    color_prueba[0] += 0.1
+if color_prueba[1] == 0:
+    color_prueba[1] += 0.1
+if color_prueba[2] == 0:
+    color_prueba[2] += 0.1
+
+dataframe = dataframe[['Principal_color', 'CSS_name', 'R', 'G', 'B']]
+
+dataframe = dataframe.query(
+    'R/@color_prueba[0] <=1.34 and R/@color_prueba[0] >=0.73 ')
+dataframe = dataframe.query(
+    'G/@color_prueba[1] <=1.34 and G/@color_prueba[1] >=0.73 ')
+dataframe = dataframe.query(
+    'B/@color_prueba[2] <=1.34 and B/@color_prueba[2] >=0.73 ')
+
+
+print(dataframe)
