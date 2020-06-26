baa662c412eeff4ea01aaabb11147e5cda370124
matias1379 <52383409+matias1379@users.noreply.github.com>
2020-06-26 19:02:25 -0300

    Descarga blanco y negro para el query y responde automatico

--- la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
+++ la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
@@ -16,14 +16,41 @@ if color_prueba[1] == 0:
 if color_prueba[2] == 0:
     color_prueba[2] += 0.1
 
+mx = max(color_prueba[0]  , color_prueba[1]  , color_prueba[2])
+mn = min(color_prueba[0]  , color_prueba[1]  , color_prueba[2])
+
+diferencia_min_max= mx - mn
+
+
+if mx <= 50 and diferencia_min_max <= 5 :
+    print("color: black")
+
+elif mn >= 230 : 
+    print("color: blanco")
+
+else:
+    pass
+
+
+
+
+
 dataframe=dataframe[['Principal_color', 'CSS_name', 'R', 'G', 'B']]
 
-dataframe=dataframe.query(
+
+
+df=dataframe.query(
     'R/@color_prueba[0] <=1.34 and R/@color_prueba[0] >=0.73 ')
-dataframe=dataframe.query(
+df=dataframe.query(
     'G/@color_prueba[1] <=1.34 and G/@color_prueba[1] >=0.73 ')
-dataframe=dataframe.query(
+df=dataframe.query(
     'B/@color_prueba[2] <=1.34 and B/@color_prueba[2] >=0.73 ')
+
+
+if(df.empty()):
+    print("no se encontro una opcion en la base de datos")
+    
+
 
 
 print(dataframe)
