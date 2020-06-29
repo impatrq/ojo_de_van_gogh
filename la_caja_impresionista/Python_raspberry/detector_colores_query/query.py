dfdc26fb9739d9dfbcb84781d22eb7fde84e40e9
matias1379 <52383409+matias1379@users.noreply.github.com>
2020-06-29 17:20:33 -0300

    Se puso un if que detecta cuando el query esta vacio. En este caso hace un append al dataframe original para agregarle el nuevo color y deja un mensaje de que hay que clasificarlo. Machine learning papap

--- la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
+++ la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
@@ -5,9 +5,7 @@ import array
 dataframe = pd.read_csv('Tabla_colores.csv')
 
 
-
-
-color_prueba=[239, 127, 26]
+color_prueba = [600, 900, 888]
 
 if color_prueba[0] == 0:
     color_prueba[0] += 0.1
@@ -16,41 +14,56 @@ if color_prueba[1] == 0:
 if color_prueba[2] == 0:
     color_prueba[2] += 0.1
 
-mx = max(color_prueba[0]  , color_prueba[1]  , color_prueba[2])
-mn = min(color_prueba[0]  , color_prueba[1]  , color_prueba[2])
+mx = max(color_prueba[0], color_prueba[1], color_prueba[2])
+mn = min(color_prueba[0], color_prueba[1], color_prueba[2])
 
-diferencia_min_max= mx - mn
+diferencia_min_max = mx - mn
 
 
-if mx <= 50 and diferencia_min_max <= 5 :
+if mx <= 50 and diferencia_min_max <= 5:
     print("color: black")
 
-elif mn >= 230 : 
+elif mn >= 230:
     print("color: blanco")
 
 else:
     pass
 
 
+dataframe = dataframe[['Principal_color', 'CSS_name', 'R', 'G', 'B']]
 
 
-
-dataframe=dataframe[['Principal_color', 'CSS_name', 'R', 'G', 'B']]
-
-
-
-df=dataframe.query(
+df = dataframe.query(
     'R/@color_prueba[0] <=1.34 and R/@color_prueba[0] >=0.73 ')
-df=dataframe.query(
+df = dataframe.query(
     'G/@color_prueba[1] <=1.34 and G/@color_prueba[1] >=0.73 ')
-df=dataframe.query(
+df = dataframe.query(
     'B/@color_prueba[2] <=1.34 and B/@color_prueba[2] >=0.73 ')
 
 
-if(df.empty()):
+if(df.empty == True):
     print("no se encontro una opcion en la base de datos")
-    
+    df = pd.DataFrame(columns=['Principal_color', 'CSS_name', 'R', 'G', 'B'])
+    dataframe = dataframe.append(
+        dict(
+            Principal_color="Clasificar",
+            CSS_name="Clasificar",
+            R=color_prueba[0],
+            G=color_prueba[1],
+            B=color_prueba[2],
+        ), ignore_index=True
+    )
+    dataframe = dataframe.append(
+        dict(
+            Principal_color="Clasificar",
+            CSS_name="Clasificar",
+            R=color_prueba[0],
+            G=color_prueba[1],
+            B=color_prueba[2],
+        ), ignore_index=True
+    )
+
+    print(dataframe)
 
 
-
-print(dataframe)
+print(df)
