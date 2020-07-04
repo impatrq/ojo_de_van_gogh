d6d6c3700812072358a0ef0c3b16dc6a06d8d36f
matias1379 <52383409+matias1379@users.noreply.github.com>
2020-07-03 21:23:01 -0300

    ya usa busqueda binaria en uno de los 3 query

--- la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
+++ la_caja_impresionista/Python_raspberry/detector_colores_query/query.py
@@ -4,27 +4,27 @@ import array
 
 dataframe = pd.read_csv('Tabla_colores.csv')
 
-print(dataframe)
-color_prueba = [0, 0, 0]
 
-def busqueda_binaria(lista, comienzo, final, objetivo):
+color_prueba = [255, 228, 225]
+
+def busqueda_binaria(dataframe, comienzo, final, objetivo):
 
     print("entre")
 
 
 
-    print(lista)
-    if comienzo > final:
-        return False
 
+    if comienzo > final:
+        return "hola"
     medio = (comienzo + final) // 2
 
-    if lista[medio] == objetivo:
-        return True
-    elif lista[medio] < objetivo:
-        return busqueda_binaria(lista, medio + 1, final, objetivo)
+    if dataframe['R'][medio] == objetivo:
+        print("objetivo")
+        return objetivo
+    elif dataframe['R'][medio] < objetivo:
+        return busqueda_binaria(dataframe, medio + 1, final, objetivo)
     else:
-        return busqueda_binaria(lista, comienzo, medio - 1, objetivo)
+        return busqueda_binaria(dataframe, comienzo, medio - 1, objetivo)
 
 if color_prueba[0] == 0:
     color_prueba[0] += 0.1
@@ -50,12 +50,7 @@ else:
 
 
 dataframe = dataframe[['Principal_color', 'CSS_name', 'R', 'G', 'B']]
-c = 0
-lista = []
 
-while c < 140:
-    lista.append(dataframe['R'][c])
-    c+=1
     
 
 
@@ -63,38 +58,38 @@ while c < 140:
 
 
 
-Resultado_busqueda_binaria = busqueda_binaria(lista,0 , len(dataframe[['R']]),color_prueba[0]) 
+Resultado_busqueda_binaria = busqueda_binaria(dataframe,0 , len(dataframe[['R']]),color_prueba[0]) 
 
-df = dataframe.where(Resultado_busqueda_binaria,inplace = True)
-# df = dataframe.query(
-#     'G/@color_prueba[1] <=1.34 and G/@color_prueba[1] >=0.73 ')
-# df = dataframe.query(
-#     'B/@color_prueba[2] <=1.34 and B/@color_prueba[2] >=0.73 ')
-
-
-if(df.empty == True):
-    print("no se encontro una opcion en la base de datos")
-    df = pd.DataFrame(columns=['Principal_color', 'CSS_name', 'R', 'G', 'B'])
-    dataframe = dataframe.append(
-        dict(
-            Principal_color="Clasificar",
-            CSS_name="Clasificar",
-            R=color_prueba[0],
-            G=color_prueba[1],
-            B=color_prueba[2],
-        ), ignore_index=True
-    )
-    dataframe = dataframe.append(
-        dict(
-            Principal_color="Clasificar",
-            CSS_name="Clasificar",
-            R=color_prueba[0],
-            G=color_prueba[1],
-            B=color_prueba[2],
-        ), ignore_index=True
-    )
-    dataframe.to_csv('Tabla_colores.csv',index=False)
-    print(dataframe)
+dataframe.query('R == @Resultado_busqueda_binaria',inplace=True)
+dataframe.query(
+     'G/@color_prueba[1] <=1.05 and G/@color_prueba[1] >=0.95 ',inplace= True)
+dataframe.query(
+     'B/@color_prueba[2] <=1.05 and B/@color_prueba[2] >=0.95 ', inplace= True)
 
 
-print(df)
+# if(df.empty == True):
+#     print("no se encontro una opcion en la base de datos")
+#     df = pd.DataFrame(columns=['Principal_color', 'CSS_name', 'R', 'G', 'B'])
+#     dataframe = dataframe.append(
+#         dict(
+#             Principal_color="Clasificar",
+#             CSS_name="Clasificar",
+#             R=color_prueba[0],
+#             G=color_prueba[1],
+#             B=color_prueba[2],
+#         ), ignore_index=True
+#     )
+#     dataframe = dataframe.append(
+#         dict(
+#             Principal_color="Clasificar",
+#             CSS_name="Clasificar",
+#             R=color_prueba[0],
+#             G=color_prueba[1],
+#             B=color_prueba[2],
+#         ), ignore_index=True
+#     )
+#     dataframe.to_csv('Tabla_colores.csv',index=False)
+#     print(dataframe)
+
+
+print(dataframe)
