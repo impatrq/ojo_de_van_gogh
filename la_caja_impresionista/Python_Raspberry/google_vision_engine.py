1a8abae0c6b18f4939de3f1c5c52b152111be623
matias1379 <52383409+matias1379@users.noreply.github.com>
2020-07-09 22:19:11 -0300

    Se comento el metodo de reconocer un objeto

--- la_caja_impresionista/Python_Raspberry/google_vision_engine.py
+++ la_caja_impresionista/Python_Raspberry/google_vision_engine.py
@@ -14,35 +14,47 @@ from textblob import TextBlob
 
 class GoogleVisionEngine:
 
+    #Se crea el metodo constructor que pide la imagen para trabajar
+    #con los metodos
     def __init__(self, image_path):
-        self.image_path = image_path
+        self.image_path = image_path #Al objeto se le agrega el valor de path que le mandamos
+        
 
+    #Metodo para reconocer un objeto
     def reconocer_objeto(self, ):
-        client = vision.ImageAnnotatorClient()
+        client = vision.ImageAnnotatorClient() #creamos el cliente de google vision
 
         with open(self.image_path, 'rb') as image:
-            content = image.read()
+            content = image.read() # Leemos las cosas de la imagen y lo guardamos en contenido
+            # Enviamos la peticion a la API de google con el formato JSON
+            # especifico optimizar la velocidad de respuesta
             response = client.annotate_image({'image': {'content': content}, 'features': [
-                                             {'type': vision.enums.Feature.Type.OBJECT_LOCALIZATION}], })
-            localized_object_annotations = response.localized_object_annotations
+                                             {'type': vision.enums.Feature.Type.OBJECT_LOCALIZATION}], }) 
+            localized_object_annotations = response.localized_object_annotations #Guardamos la respuesta de la API
 
         df_reconocer_objeto = pd.DataFrame(
-            columns=['name', 'score', 'bounding'])
+            columns=['name', 'score', 'bounding']) #Creamos un dataframe con la respuesta de la API
 
+        #Hacemos un for que analiza cada objeto de todos los que habia en la respuesta de la API
         for obj in localized_object_annotations:
             df_reconocer_objeto = df_reconocer_objeto.append(
                 dict(
-                    name=obj.name,
-                    score=obj.score,
-                    bounding=obj.bounding_poly
+                    name=obj.name, # Nombre del objeto 
+                    score=obj.score,# Confiabilidad de que el objeto sea correcto
+                    bounding=obj.bounding_poly #Sirve para saber los vertices del objeto y poder ubicarlo
                 ),
                 ignore_index=True)
-            df_reconocer_objeto = df_reconocer_objeto.query('score >= 0.65')
 
+            #Descartamos los objetos de poca probabilidad de que sean correctos
+            df_reconocer_objeto = df_reconocer_objeto.query('score >= 0.65') 
+
+            #Si el valor es confiable
             if (obj.score >= 0.65):
-
+                
+                #Posicion en X
                 promedio_x = (
                     obj.bounding_poly.normalized_vertices[0].x + obj.bounding_poly.normalized_vertices[1].x)/2
+                #Posicion en Y
                 promedio_y = (
                     obj.bounding_poly.normalized_vertices[0].y + obj.bounding_poly.normalized_vertices[2].y)/2
 
@@ -75,15 +87,19 @@ class GoogleVisionEngine:
 
     def pedir_color(self, dataframe_colores):
 
-        client = vision.ImageAnnotatorClient()
-        dataframe_colores = dataframe_colores
+        client = vision.ImageAnnotatorClient() #creamos el cliente de google vision
+        dataframe_colores = dataframe_colores #guardamos la tabla de colores patrones para el query
 
         with open(self.image_path, 'rb') as image:
-            content = image.read()
+            content = image.read()# Leemos las cosas de la imagen y lo guardamos en contenido
+            # Enviamos la peticion a la API de google con el formato JSON
+            # especifico optimizar la velocidad de respuesta
             response = client.annotate_image({'image': {'content': content}, 'features': [
                                              {'type': vision.enums.Feature.Type.IMAGE_PROPERTIES}], }).image_properties_annotation
-            dominant_colors = response.dominant_colors.colors
+            dominant_colors = response.dominant_colors.colors# Respuesta de colores
 
+
+        #Ciclo for para analizar cada color que nos dio la respuesta
         for color in dominant_colors:
             df_pixel_fraction = df_pixel_fraction.append(
                 dict(
@@ -96,11 +112,12 @@ class GoogleVisionEngine:
                 ignore_index=True)
 
             df_pixel_fraction = df_pixel_fraction.sort_values(
-                ['pixel_fraction'], ascending=False).head(2)
+                ['pixel_fraction'], ascending=False).head(2) # Agarramos los dos valores con mas predominancia en la pantalla
             df_pixel_fraction = df_pixel_fraction.sort_values(
-                ['score'], ascending=False).head(2)
-        dif = df_pixel_fraction['score'][0] - df_pixel_fraction['score'][1]
+                ['score'], ascending=False).head(2) # Agarramos valores de confiabilidad altos
+        dif = df_pixel_fraction['score'][0] - df_pixel_fraction['score'][1] # Nos fiajmos si el primer color y el segundo son ambos confiables 
 
+        #Si los dos colores son confaibles usamos los dos
         if (dif < 0.10):
             cont = 0
             for cont in [0, 1]:
@@ -108,19 +125,26 @@ class GoogleVisionEngine:
                 g = (df_pixel_fraction['g'][cont])
                 b = (df_pixel_fraction['b'][cont])
 
-                self.rgb_to_name_query(dataframe_colores, r, g, b)
-
+                self.rgb_to_name_query(dataframe_colores, r, g, b)# Llamamos a la funcion que hace el query para el nombre de color
+        #Si solo hay uno confiable usamos ese y ya esta
         else:
             r = (df_pixel_fraction['r'][0])
             g = (df_pixel_fraction['g'][0])
             b = (df_pixel_fraction['b'][0])
 
-            self.rgb_to_name_query(dataframe_colores, r, g, b)
+            self.rgb_to_name_query(dataframe_colores, r, g, b)# Llamamos a la funcion que hace el query para el nombre de color
 
     def rgb_to_name_query(self, dataframe_colores, R, G, B):
 
-        dataframe_colores = dataframe_colores
-        R = R
+        dataframe_colores = dataframe_colores #Guardamos la tabla de colores con nombres
+
+        #Guardamos los valores de R,G,B
+        R = R 
+        G = G
+        B = B
+
+        #Como no se puede dividir por 0 le agregamos 0.1 si es 
+        #0 el valor que entra
 
         if R == 0:
             R += 0.1
@@ -129,17 +153,22 @@ class GoogleVisionEngine:
         if B == 0:
             B += 0.1
 
+        #Nos fijamos los valores que entran su minimo y maximo
         mx = max(R, G, B)
         mn = min(R, G, B)
 
         diferencia_min_max = mx - mn
-
+        # Si los tres valores son muy parecidos y son menores a 50
+        # Siempre da negro la combinacion
         if mx <= 50 and diferencia_min_max <= 5:
             print("color: black")
-
+        
+        # Si los tres valores son muy parecidos y son mayores a 230
+        # Siempre da blanco la combinacion
         elif mn >= 230:
             print("color: blanco")
 
+        #Si no procedemos a el query
         else:
             dataframe_colores = dataframe_colores[[
                 'Principal_color', 'CSS_name', 'R', 'G', 'B']]
