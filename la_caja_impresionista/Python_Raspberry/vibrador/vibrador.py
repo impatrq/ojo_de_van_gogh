9a3fa92fbf4eccc7e38b0eaf170af4b7754cdf4f
NahuCarro <nahuel.carro001@gmail.com>
2020-07-19 19:55:14 -0300

    Se creo el codigo para probar la clase
    Se hizo el codigo para probar las vibraciones segun el color que ingrese y se borro la libreria rpi porque no es necesaria

--- la_caja_impresionista/Python_Raspberry/vibrador/vibrador.py
+++ la_caja_impresionista/Python_Raspberry/vibrador/vibrador.py
@@ -1,2 +1,6 @@
-import RPi.GPIO as GPIO
 from morse_colores import morse_colores
+# guardamos en una variable el pin de salida
+
+vibracion = morse_colores(22)
+color = "rojo"
+vibracion.color_to_sonido(color)
