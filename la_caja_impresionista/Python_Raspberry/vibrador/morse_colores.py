807bf6280ab7c35c33bd5f98337fca12a94942eb
NahuCarro <nahuel.carro001@gmail.com>
2020-07-19 19:53:40 -0300

    Se agrego la funcion de color_to_sonido
    Se creo una funcion la cual devuelve la vibracion correspondiente segun el nombre del color que le ingrese

--- la_caja_impresionista/Python_Raspberry/vibrador/morse_colores.py
+++ la_caja_impresionista/Python_Raspberry/vibrador/morse_colores.py
@@ -4,15 +4,23 @@ import time
 
 class morse_colores:
     pin_buzzer = int
-    frecuencia = int
 
     # Se crea el metodo que pide el pin en el que saldra la vibracion
     def __init__(self, pin_buzzer):
         # se agrega el valor de pin
         self.pin_buzzer = pin_buzzer
-        pass
+
+        # desactivamos mensajes de error
+        GPIO.setwarnings(False)
+
+        # indicamos el uso de  la identificacion BCM para los GPIO
+        GPIO.setmode(GPIO.BCM)
+
+        # indicamos que el GPIO18 es de salida
+        GPIO.setup(self.pin_buzzer, GPIO.OUT)
 
     # Se crea la funcion para hacer un punto
+
     def punto(self):
         # Se activa la salida por 250mS
         GPIO.output(self.pin_buzzer, True)
@@ -102,3 +110,52 @@ class morse_colores:
         self.linea()
         self.linea()
         self.linea()
+
+    def color_to_sonido(self, color):
+        if (color == "blanco"):
+            self.blanco()
+            print("Color blanco")
+
+        elif (color == "rojo"):
+            self.rojo()
+            print("Color rojo")
+
+        elif (color == "marron"):
+            self.marron()
+            print("Color marron")
+
+        elif (color == "naranja"):
+            self.naranja()
+            print("Color naranja")
+
+        elif (color == "amarillo"):
+            self.amarillo()
+            print("Color amarillo")
+
+        elif (color == "verde"):
+            self.verde()
+            print("Color verde")
+
+        elif (color == "cyan"):
+            self.cyan()
+            print("Color cyan")
+
+        elif (color == "azul"):
+            self.azul()
+            print("Color azul")
+
+        elif (color == "violeta"):
+            self.violeta()
+            print("Color violeta")
+
+        elif (color == "rosa"):
+            self.rosa()
+            print("Color rosa")
+
+        elif (color == "gris"):
+            self.gris()
+            print("Color gris")
+
+        elif (color == "negro"):
+            self.negro()
+            print("Color negro")
