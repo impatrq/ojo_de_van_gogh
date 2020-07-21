1c1111c86ca796026b1fc4d4a8107c3233c238a6
NahuCarro <nahuel.carro001@gmail.com>
2020-07-21 16:47:29 -0300

    Se creo la clase que pasa de texto a audio
    La clase recibe un string como texto y lo reproduce

---
+++ la_caja_impresionista/Python_Raspberry/texto_to_audio.py
@@ -0,0 +1,33 @@
+import os
+import io
+from gtts import gTTS
+
+# Se crea el metodo y se declara la variable
+
+
+class texto_to_audio:
+    texto = str
+
+    def __init__(self):
+        pass
+
+    # Se crea la funcion que pasa de texto a audio
+    def audio(self, texto):
+        self.texto = texto
+
+        # Se crea un archivo txt que tiene la variable texto
+        with open('texto_a_traducir.txt', 'w') as f:
+                    f.write(self.texto)
+
+                # Se lee el archivo creado
+                with open('texto_a_traducir.txt') as f:
+                    lines = f.read()
+
+                    # se convierte ese archivo a audio
+                    output = gTTS(text = lines, lang = 'es', slow = False)
+
+                    # Se guarda el audio como mp3
+                    output.save('texto.mp3')
+
+                # Se reproduce el audio
+                os.system('mpg321 texto.mp3 &')
